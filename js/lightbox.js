9eb1975c844672187fb3122f3a8f3b252db91d3d
balmain <balmainctf@126.com>
2016-10-09 11:18:55 +0800

    html css

---
+++ js/lightbox.js
@@ -0,0 +1,18 @@
+;(function($){
+	alert(1);
+	var LightBox = function(){
+		var self = this;
+	};
+	LightBox.prototype={
+
+	};
+	window["LightBox"] = LightBox;
+
+})(jQuery)
+
+
+$ git config --global user.name "marshal"
+$ git config --global user.email "kidsover200891@qq.com"
+ssh-keygen -t rsa -C "kidsover200891@qq.com"
+cp 123456* key_backup
+git remote add origin git@github.com:balmainctf/lightBox.git
